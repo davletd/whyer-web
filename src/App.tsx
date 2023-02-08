c649b4445ad2b850d971d9c5bbea75b50cd63cbd
Davlet Dzhakishev <davlet@fjong.com>
2023-02-08 20:51:22 +0000

    Add material ui example

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import React from 'react';
-import logo from './logo.svg';
+
+import TextField from '@mui/material/TextField';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
+          <TextField id="Question" multiline={true} />
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
