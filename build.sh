223876835477e13b1f750948619b9381ed50e0e6
Davlet Dzhakishev <davlet@fjong.com>
2023-02-12 21:46:49 +0000

    Serve react app

---
+++ build.sh
@@ -0,0 +1,2 @@
+GCP_PROJECT="personal-sandbox-1333"
+gcloud builds submit --tag gcr.io/$GCP_PROJECT/helloworld
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+GCP_PROJECT="personal-sandbox-1333"
+gcloud run deploy --image gcr.io/$GCP_PROJECT/helloworld
