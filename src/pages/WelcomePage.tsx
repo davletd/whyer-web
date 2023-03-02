e358366b23c3b4e8ae6615be5db9b73bdba640e4
Davlet Dzhakishev <davlet@fjong.com>
2023-03-02 21:10:39 +0100

    Fix styles for Welcome page

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
+import styles from './WelcomePage.module.scss';
 
 const WelcomePage = () => {
-	return <React.Fragment>
-		<img src="Nesh.png" alt="Welcome to WHYer" />
-	</React.Fragment>
+
+	const welcomeText = `Welcome heartly to WHYer! 
+	WHYer is a AI-powered learning companion. I can answer any questions, help learn concepts,
+	check your homework and many more things. Just give it a try!`;
+
+	return (
+		<React.Fragment>
+
+		
+		<div className={styles.Body}>
+			<div className={styles.Image}><img src="Nesh.png" alt="Welcome to WHYer" /></div>
+			<div className={styles.SpeechBubble}>{welcomeText}</div>
+		</div>
+		
+		</React.Fragment>
+	)
 }
 export default WelcomePage;
