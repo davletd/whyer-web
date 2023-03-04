37ace35c738fc67839aa4c0a32991c1135c1646c
Davlet Dzhakishev <davlet@fjong.com>
2023-03-04 12:38:40 +0100

    Update questions page using ionic

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css'; // Remove if nothing is visible
 import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
@@ -24,11 +23,10 @@ import { setupIonicReact } from '@ionic/react';
 import React, { useState } from 'react';
 import axios from "axios";
 
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
 import './App.scss';
 import WelcomePage from './pages/WelcomePage';
 import HowOldAreYouPage from './pages/HowOldAreYouPage';
+import QuestionsPage from './pages/QuestionsPage';
 
 setupIonicReact();
 
@@ -36,23 +34,7 @@ const App = () => {
   const [seenWelcomePage, setSeenWelcomePage] = useState(false);
   const [confirmAge, setConfirmAge] = useState(false);
   const [yourAge, setYourAge] = useState(0);
-  const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    // axios
-    //   .post("https://test-nodejs-ozzd3ccvdq-uw.a.run.app/chat", { prompt })
-    //   .then((res) => {
-    //     // Update the response state with the server's response
-    //     setResponse(res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
-    setResponse("My answer is long enough to show up. My answer is long enough to show up. My answer is long enough to show up. My answer is long enough to show up. My answer is long enough to show up.");
-  }
-
+ 
   if (!seenWelcomePage) {
     return <WelcomePage setSeenWelcomePage={setSeenWelcomePage} />
   }
@@ -62,30 +44,7 @@ const App = () => {
   }
 
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="whyer.png" alt="Welcome to WHYer" />
-      </header>
-      <div className="App-body">
-       <form className="App-form" onSubmit={handleSubmit}>
-        <label className="label-text-input" htmlFor="text-input">Ask your question here:</label>
-        <div className="text-field-input">
-          <TextField
-            fullWidth={true}
-            type="text"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
-          />
-        </div>
-        <Button type="submit" size="large" variant="contained">Submit</Button>
-        </form>
-        <div className="result-text-container">
-          <p>{response}</p>
-        </div>
-      </div>
-    </div>
-  );
+  return <QuestionsPage />;
 }
 
 export default App;
---
+++ src/pages/QuestionsPage.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import axios from "axios";
+import { IonButton, IonItem, IonTextarea } from '@ionic/react';
+
+const QuestionsPage = () => {
+
+	const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    // axios
+    //   .post("https://test-nodejs-ozzd3ccvdq-uw.a.run.app/chat", { prompt })
+    //   .then((res) => {
+    //     // Update the response state with the server's response
+    //     setResponse(res.data);
+    //   })
+    //   .catch((err) => {
+    //     console.error(err);
+    //   });
+    setResponse(prompt);
+  }
+
+
+	return (
+		<div className="App">
+		<header className="App-header">
+			<img src="whyer.png" alt="Welcome to WHYer" />
+		</header>
+		<div className="App-body">
+		 <form className="App-form" onSubmit={handleSubmit}>
+			<label className="label-text-input" htmlFor="text-input">Ask your question here:</label>
+			<div className="text-field-input">
+			<IonItem>
+				<IonTextarea
+					placeholder="Type something here"
+					autoGrow={true}
+					
+					onIonInput={(e) => setPrompt(e.target.value || "")}
+				></IonTextarea>
+			</IonItem>
+			
+			</div>
+			<IonButton type="submit" size="large">Submit</IonButton>
+			</form>
+			<div className="result-text-container">
+				<p>{response}</p>
+			</div>
+		</div>
+	</div>
+	)
+}
+
+export default QuestionsPage;
