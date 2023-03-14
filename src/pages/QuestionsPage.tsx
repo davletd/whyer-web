4c5bed678e95a7b8966bcd039b8f042e3c0162d7
Davlet Dzhakishev <davlet@fjong.com>
2023-03-14 18:05:12 +0100

    Update questions

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -7,29 +7,33 @@ const QuestionsPage = (props: any) => {
 
 	const yourAge = props.yourAge;
 	const [prompt, setPrompt] = useState("");
-  const [response, setResponse] = useState("");
+	const [response, setResponse] = useState([{question: "", answer: ""}]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const WhyerTextDefault = "Ask me about what is the sun or what is 2+2, I can help check your homework as well.";
+	const WhyerTextLoading = "Great question! Let me think about it for a while...";
+	const WhyerText = isLoading ? WhyerTextLoading : WhyerTextDefault;
 
   const handleSubmit = async (e: any) => {
-
+    e.preventDefault();
+		setIsLoading(true);
 		const updatedPrompt = `I am ${yourAge} year old. Give me friendly but detailed answer to the question: "${prompt} in the language of question. 
-		If it concerns sexuality, violence or other dangerous topics, please ask them to refer to their parents or teachers for this question.
-		If it is a math question, or you asked to write essay explain how it works"`;
+		If it concerns sexuality, human reproduction, violence or other dangerous topics, refer to their parents or teachers for this question."`;
 		console.log(prompt);
 		console.log(updatedPrompt);
-    e.preventDefault();
-    // axios
-    //   .post("https://us-central1-whyer-core.cloudfunctions.net/app/chat", { prompt: updatedPrompt  })
-		// 	//.post("http://localhost:5001/whyer-core/us-central1/app/chat", { prompt: updatedPrompt })
-    //   .then((res) => {
-    //     // Update the response state with the server's response
-    //     setResponse(res.data);
-    //   })
-    //   .catch((err) => {
-    //     console.error(err);
-    //   });
-		setResponse(`I am ${yourAge} year old. Give me friendly but detailed answer to the question: "${prompt} in the language of question. 
-		If it concerns sexuality, violence or other dangerous topics, please ask them to refer to their parents or teachers for this question.
-		If it is a math question, or you asked to write essay explain how it works"`);
+    axios
+      .post("https://us-central1-whyer-core.cloudfunctions.net/app/chat", { prompt: updatedPrompt  })
+			//.post("http://localhost:5001/whyer-core/us-central1/app/chat", { prompt: updatedPrompt })
+      .then((res) => {
+        // Update the response state with the server's response
+				response.unshift({question: prompt, answer: res.data})
+        setResponse(response);
+				setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error(err);
+				setIsLoading(false);
+      });
   }
 
 
@@ -37,27 +41,44 @@ const QuestionsPage = (props: any) => {
 		<div>
 		<header className={styles.Header}>
 			<img className={styles.ImageHeader} src="Nesh.png" alt="Welcome to WHYer" />
-			<div className={styles.SpeechBubble}>{"Ask me about what is the sun or what is 2+2, I can help check your homework as well."}</div>
+			<div className={styles.SpeechBubble}>{WhyerText}</div>
 		</header>
 		<div className={styles.Container}>
 		 <form className={styles.Form} onSubmit={handleSubmit}>
 			<div className={styles.TextFieldInput}>
 			<IonItem>
-				<IonTextarea
-					placeholder="Type something here"
-					autoGrow={true}
-					
-					onIonInput={(e) => setPrompt(e.target.value || "")}
-				></IonTextarea>
+			{
+				isLoading ?
+					<IonTextarea
+						placeholder={"Your questions go here. Click button below when done"}
+						autoGrow={true}
+						disabled={true}
+						onIonInput={(e) => setPrompt(e.target.value || "")}
+					></IonTextarea> :
+					<IonTextarea
+						placeholder={"Your questions go here. Click button below when done"}
+						autoGrow={true}
+						onIonInput={(e) => setPrompt(e.target.value || "")}
+					></IonTextarea>
+				}
 			</IonItem>
-			
 			</div>
-			<IonButton type="submit" size="large">Ask</IonButton>
+			{isLoading? 
+				<IonButton type="submit" size="large" disabled={true}>Thinking...</IonButton> :
+				<IonButton type="submit" size="large">Ask</IonButton>}
 			</form>
 			<div className={styles.ResultText}>
-				<img className={styles.ImageSmall} src="Nesh.png" alt="Welcome to WHYer" />
-				<div className={styles.SpeechBubbleBody}>{response}</div>
-				<div className={styles.SpeechBubbleQuestions}>{prompt}</div>
+				{response.map((conversation) => {
+					if (conversation?.answer !== '') {
+					return (
+						<div>
+							<img className={styles.ImageSmall} src="Nesh.png" alt="Welcome to WHYer" />
+							<div className={styles.SpeechBubbleBody}>{conversation?.answer}</div>
+							<div className={styles.SpeechBubbleQuestions}>{conversation?.question}</div>
+						</div>
+					);}
+				})}
+
 			</div>
 		</div>
 	</div>
