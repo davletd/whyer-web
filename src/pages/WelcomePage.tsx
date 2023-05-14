2356209e66041631e51d2a918e2af5da6cbd7afc
Davlet Dzhakishev <davlet@fjong.com>
2023-05-14 18:34:02 +0600

    Initial setup color theming and login button

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { IonPage, IonButton } from '@ionic/react';
+import { IonPage, IonButton, IonHeader, IonContent } from '@ionic/react';
 import styles from './WelcomePage.module.scss';
 import LoginButton from '../components/LoginButton';
 import { useHistory } from "react-router-dom";
@@ -13,14 +13,16 @@ const WelcomePage = () => {
 
 	return (
 		<IonPage>
-			<div className={styles.Body}>
-				<header>
+				<IonHeader>
 					<LoginButton />
-				</header>
-				<div className={styles.Image}><img src="/Nesh.png" alt="Welcome to WHYer" /></div>
-				<div className={styles.SpeechBubble}>{welcomeText}</div>
-				<IonButton className={styles.Button} onClick={() => history.push('/welcome/age')}>Continue</IonButton>
-			</div>
+				</IonHeader>
+				<div className={styles.Body}>
+				<IonContent className={styles.Body}>
+					<div className={styles.Image}><img src="/Nesh.png" alt="Welcome to WHYer" /></div>
+					<div className={styles.SpeechBubble}>{welcomeText}</div>
+					<IonButton className={styles.Button} onClick={() => history.push('/welcome/age')}>Continue</IonButton>
+				</IonContent>
+				</div>
 		</IonPage>
 	)
 }
